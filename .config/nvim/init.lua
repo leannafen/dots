f7758f9a366fa767a8d7b4ee4de668ec2f19edde
leannafen <65414487+leannafen@users.noreply.github.com>
2022-02-27 22:35:01 -0500

    Added tab stop config.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,6 +3,10 @@ local cmd = vim.cmd
 require("settings")
 require("plugins")
 
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+
 local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.on_server_ready(function(server)
     local opts = {}
